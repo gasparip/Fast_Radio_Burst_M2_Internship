269ec6e93595fefbaa78159398be3beca97cf376
Pierre Gaspari <127432750+gasparip@users.noreply.github.com>
2024-05-15 16:16:36 +0200

    Delete Project_Data_Base_Project directory

--- Project_Data_Base_Project/bashScript/NomFRBObesrvationNenufarModeTf.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-ls /databf/nenufar-tf/ES05/*/*/*/L1 | grep .fits | grep -v '_rm' | cut -d'_' -f1 | sort | uniq -c > ./FichierTestModeTF.txt
-echo 'ls /databf/nenufar-tf/ES05/*/*/*/L1 | grep .fits | grep -v '_rm' | cut -d'_' -f1 | sort | uniq -c > ./FichierTestModeTF.txt'
-
-exit 1
-
--- Project_Data_Base_Project/bashScript/puGiveFRBNenufar.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-echo 'psredit /databf2/nenufar-pulsar/{ES,LT}05/*/*/*.fits | grep  "Source name" | colrm 1 67 | sort | uniq -c' 
-psredit /databf2/nenufar-pulsar/{ES,LT}05/*/*/*.fits | grep  "Source name" | colrm 1 67 | sort | uniq -c
-exit 1
-
